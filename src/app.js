60100a67c0af09cbd3b88fdeea244b44ef38c27b
MC1398 <94994054+MC1398@users.noreply.github.com>
2024-07-15 23:32:34 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  document.getElementById("excuse").innerHTML =
+    who[Math.floor(Math.random() * 3)] +
+    " " +
+    action[Math.floor(Math.random() * 3)] +
+    " " +
+    what[Math.floor(Math.random() * 2)] +
+    " " +
+    when[Math.floor(Math.random() * 4)];
 };
